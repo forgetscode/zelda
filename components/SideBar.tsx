8b55fa62ba2b6e3bf9e1f165aefcf2727ea65faf
michael <forgetscode@gmail.com>
2022-06-30 21:29:11 -0700

    wallet button

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -51,12 +51,7 @@ const SideBar = () => {
                         </div>
                     </Link>
                     <div className='my-auto ml-auto py-1 px-1 mr-5'>
-                        <WalletMultiButton className='!bg-gray-700
-                                    text-teal-500 
-                                    rounded-xl hover:rounded-xl
-                                    transition-all duration-300 ease-linear
-                                    !text-md
-                                    ' />
+                        <WalletMultiButton></WalletMultiButton>
                     </div>
             </div>
         </div>
